1c6a6f6b07f8b0772ed30e6a3d6ab74302098ae0
Frank Ceballos <frank@Franks-Mac-mini.hsd1.tx.comcast.net>
2021-01-17 09:24:31 -0600

    create file

---
+++ portfolio-optimizer.py
@@ -0,0 +1,30 @@
+
+""" ***************************************************************************
+# * File Description:                                                         *
+# * MUse to determine the optimal portfolio along its weights.                *
+# * The original script was taken from a YouTuber with account name Computer  *
+# * Science. You can find the link to his video on the README.                *
+# *                                                                           *
+# * The contents of this script are:                                          *
+# * 1. Importing Libraries                                                    *
+# *                                                                           *
+# * --------------------------------------------------------------------------*
+# * AUTHORS(S): Frank Ceballos <frank.ceballos89@gmail.com>                   *
+# * --------------------------------------------------------------------------*
+# * DATE CREATED:January 17, 20221                                            *
+# * --------------------------------------------------------------------------*
+# * NOTES: None                                                               *
+# * ************************************************************************"""
+
+
+###############################################################################
+#                          1. Importing Libraries                             #
+###############################################################################
+import pandas as pd
+import numpy as np
+import requests
+
+
+###############################################################################
+#                          2. Load Data                           #
+###############################################################################
